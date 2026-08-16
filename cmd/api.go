67772635b99445b49958d2b28f8dd55765681857
dirac <hobbiathan@gmail.com>
2026-08-16 05:35:49 -0600

    chi mount()

--- cmd/api.go
+++ cmd/api.go
@@ -1,15 +1,40 @@
 package main
 
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
 type application struct {
 	config config
 }
 
 // run
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	return r
+}
+
 // mount
 
 type config struct {
 	addr string
-	db dbConfig
+	db   dbConfig
 }
 
 type dbConfig struct {
